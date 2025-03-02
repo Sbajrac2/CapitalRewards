cdf51e143d1e79027545fe170507f0a7cf326ac2
Nidhi Patel <162032103+nidhip26@users.noreply.github.com>
2025-03-01 21:31:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def homepage():
 def login():
     return render_template('Loginpage.html')
 
-client = genai.Client(api_key="AIzaSyBIvKviwbeNVPKqxSAT1QlZbOxLq1-_pXE")
+client = genai.Client(api_key="")
 
 @app.route("/Chatpage", methods=["GET", "POST"])
 def chatpage():
